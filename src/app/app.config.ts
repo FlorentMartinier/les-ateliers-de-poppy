45418024f307aa3c007af84e12d447ca57477607
Florent Martinier <florent.martinier@free.fr>
2026-06-02 16:09:52 +0200

    Add google analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -8,6 +9,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(
+      NgxGoogleAnalyticsModule.forRoot('G-ZBXFSCNMCM'),
+      NgxGoogleAnalyticsRouterModule
+    )
   ]
 };
